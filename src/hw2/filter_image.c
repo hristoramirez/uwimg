0117804b7dc2d9cf90ff075d530201db3f666c29
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-01-19 16:26:52 -0800

    current workpoint

--- src/hw2/filter_image.c
+++ src/hw2/filter_image.c
@@ -8,19 +8,59 @@
 
 void l1_normalize(image im)
 {
-    // TODO
+    for (int row = 0; row < im.w; row++) {
+        for (int col = 0; col < im.h; col++) {
+            for (int i = 0; i < im.c; i++) {
+                float val = 1.0 / (im.w * im.h);
+                set_pixel(im, row, col, i, val);
+            }
+        }
+    }
 }
 
 image make_box_filter(int w)
 {
-    // TODO
-    return make_image(1,1,1);
+    image box_filter = make_image(w, w, 1);
+    l1_normalize(box_filter);
+    return box_filter;
 }
 
 image convolve_image(image im, image filter, int preserve)
 {
-    // TODO
-    return make_image(1,1,1);
+    // Check that filter has 1 or the same number
+    // of channels as im
+    assert(im.c == filter.c || filter.c == 1);
+    float q;
+    image result;
+    image temp;
+
+    // Make image depending on preserve
+    if (preserve) {
+        result = make_image(im.w, im.h, im.c);
+    } else {
+        result = make_image(im.w, im.h, 1);
+    }
+
+    if (filter.c == im.c) {
+        for (int i = 0; i < im.c; i++) {
+            for (int row = 0; row < im.h; row++) {
+                for (int col = 0; col < im.w; col++) {
+                    q = 0.0;
+                    for (int fy = 0; fy < filter.h; fy++) {
+                        for (int fx = 0; fx < filter.w; fx++) {
+                            int x = col - ( (filter.w / 2) - fx );
+                            int y = row - ( (filter.h / 2) - fy );
+                            float f_val = get_pixel(filter, x, y, i);
+                            float i_val = get_pixel(im, col, row, i);
+                            set_pixel(result, col, row, i, f_val * i_val);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
 image make_highpass_filter()
